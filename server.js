c3424440475e11344253297317614a8a2e6a929d
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-04-29 09:56:25 +0530

    server  modified limit,ddos,apiroute

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const connectDB = require("./config/db");
+
 const apiRoutes = require("./routes/apiRoutes");
 const limiter = require("./middleware/rateLimiter");
 const ddosProtection = require("./middleware/ddosProtection");
+const logger = require("./middleware/logger");
 
 const app = express();
 
+connectDB();
+
 app.use(express.json());
-app.use(morgan("combined")); 
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
-  res.send(" Secure API Gateway Running");
+  res.send("API Gateway Running");
 });
 
+app.use(logger);
 app.use(ddosProtection);
 app.use("/api", limiter);
 app.use("/api", apiRoutes);
