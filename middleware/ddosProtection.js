3e757128ff731266211dc0b6d68a09ce532e9138
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-04-29 22:02:15 +0530

    updated version

--- middleware/ddosProtection.js
+++ middleware/ddosProtection.js
@@ -1,4 +1,4 @@
-const Log = require("../models/Log");
+const logRequest = require("../utils/logger");
 
 const requestCounts = {};
 const blockedIPs = new Map();
@@ -6,31 +6,26 @@ const blockedIPs = new Map();
 const ddosProtection = async (req, res, next) => {
   const ip = req.ip;
 
-  if (blockedIPs.has(ip)) {
-    if (Date.now() < blockedIPs.get(ip)) {
-      await Log.create({
-        ip,
-        endpoint: req.originalUrl,
-        method: req.method,
-        status: "blocked"
-      });
-      return res.status(403).send("Blocked");
-    } else {
-      blockedIPs.delete(ip);
+  try {
+    if (blockedIPs.has(ip)) {
+      if (Date.now() < blockedIPs.get(ip)) {
+        await logRequest(req, "blocked");
+        return res.status(403).send("Blocked");
+      } else {
+        blockedIPs.delete(ip);
+      }
     }
-  }
 
-  requestCounts[ip] = (requestCounts[ip] || 0) + 1;
+    requestCounts[ip] = (requestCounts[ip] || 0) + 1;
 
-  if (requestCounts[ip] > 20) {
-    blockedIPs.set(ip, Date.now() + 5 * 60 * 1000);
-
-    await Log.create({
-      ip,
-      endpoint: req.originalUrl,
-      method: req.method,
-      status: "suspicious"
-    });
+    if (requestCounts[ip] > 20) {
+      blockedIPs.set(ip, Date.now() + 5 * 60 * 1000);
+      await logRequest(req, "suspicious");
+    } else {
+      await logRequest(req, "normal");
+    }
+  } catch (error) {
+    console.log("DDoS middleware error:", error.message);
   }
 
   next();
