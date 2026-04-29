b906876f68e877effd7db85e8e4f173b0407e2e8
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-04-29 09:50:47 +0530

    role 1

---
+++ middleware/role.js
@@ -0,0 +1,10 @@
+const roleCheck = (role) => {
+  return (req, res, next) => {
+    if (req.user.role !== role) {
+      return res.status(403).send("Access denied");
+    }
+    next();
+  };
+};
+
+module.exports = roleCheck;
