d8ca3c8ccfb76baff2b5bda0def609b647e6de5a
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-05-01 20:49:45 +0530

    rate_limiter updated

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -2,8 +2,12 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 60 * 1000,
-  max: 10,
-  message: "Too many requests"
+  max: 20,
+  skip: (req) => {
+    return req.originalUrl.includes("/logs") ||
+           req.originalUrl.includes("/stats") ||
+           req.originalUrl.includes("/top-attackers");
+  }
 });
 
 module.exports = limiter;
