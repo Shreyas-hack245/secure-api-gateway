14195e9327ea2fcc4999d965c877034272b14f1d
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-05-01 20:35:02 +0530

    ip feature added

--- utils/logger.js
+++ utils/logger.js
@@ -1,15 +1,20 @@
 const Log = require("../models/Log");
+const geoip = require("geoip-lite");
 
-const logRequest = async (req, status = "normal") => {
+const logRequest = async (req, status) => {
   try {
+    const ip = req.ip === "::1" ? "8.8.8.8" : req.ip; // fix for local testing
+    const geo = geoip.lookup(ip);
+
     await Log.create({
       ip: req.ip,
       endpoint: req.originalUrl,
       method: req.method,
-      status
+      status,
+      country: geo?.country || "unknown"
     });
   } catch (error) {
-    console.log("Log error:", error.message);
+    console.log(error.message);
   }
 };
 
